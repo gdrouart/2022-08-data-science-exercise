ac5e96e8a27052d14d72898329d9a51686ba03a0
Guillaume Drouart <drouart.guillaume@gmail.com>
2022-09-10 22:15:42 +0800

    script to reduce to hourly cadence

--- source/utilities.py
+++ source/utilities.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import glob
 import os
-
+import math
 
 def read_folder(foldername):
     """Read the entire month folder and return a dataframe"""
@@ -15,3 +15,26 @@ def read_folder(foldername):
 
     return pd.concat(list_file, axis=0, ignore_index=True)
 
+def reduce_hourly(frame):
+    """
+    Take in the full frame and reduce to hourly cadence, 
+    replacing parameters values as required (+-10min or NaN)
+    """
+
+    tmp_timestamp=pd.to_datetime(frame["observe_time"])
+    hourly_frame=frame[tmp_timestamp[tmp_timestamp.dt.minute==0].index]
+
+    # check each row, if nan, check 10min earlier or 10min later and fill frame
+    for i,elem in enumerate(hourly_frame):
+        if math.isnan(elem):
+            current_index = hourly_frame['a1_0'].index[i]
+            if ~math.isnan(frame.iloc[current_index-1]['a1_0']):
+                hourly_frame.iloc[i,1:]=frame.iloc[current_index-1,1:]
+            elif ~math.isnan(frame.iloc[current_index+1]['a1_0']):
+                hourly_frame.iloc[i,1:]=frame.iloc[current_index+1,1:]
+            else:
+                hourly_frame.iloc[i,1:]=np.nan
+
+    # TODO: check graphics it is all good
+
+    return hourly_frame
